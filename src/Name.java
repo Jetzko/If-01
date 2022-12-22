9ea5419bc473c528ba787e4f4509e1c566f4e3b0
Jetzko <andreadefilippi68@gmail.com>
2022-12-22 15:32:04 +0100

    if 01

---
+++ src/Name.java
@@ -0,0 +1,11 @@
+public class Name {
+    public static void main(String[] args) {
+        String name = "Andrea";
+    if ((name.length()) %2 == 0){
+        System.out.println("true");
+    }else {
+        System.out.println("false");
+    }
+    }
+
+}
